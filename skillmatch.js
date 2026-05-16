4165d5d57afdd0c99ce92882db1951febf8534cc
Jessica Carrilho <jessicavasconcelos.suporte@gmail.com>
2026-05-16 13:56:51 -0300

    Criado objeto Candidato e Array de vagas

---
+++ skillmatch.js
@@ -0,0 +1,36 @@
+const candidato = {
+    nome: "Mariana",
+    area: "Front-End",
+    habilidades: ["JavaScript", "GitHub", "Lógica de programação", "Kanban"],
+experienciaMeses: 3
+};
+
+const vagas = [
+    {
+    id: 1,
+    empresa: "TechStart",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "GitHub", "Lógica de programação"],
+    salario: 2800,
+    modalidade: "Remoto",
+    experienciaMinimaMeses: 2
+},
+{
+    id: 2,
+    empresa: "CodeLab",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "Kanban", "GitHub"],
+    salario: 1500,
+    modalidade: "Híbrido",
+    experienciaMinimaMeses: 0
+},
+{
+    id: 3,
+    empresa: "WebSolutions",
+    cargo: "Programador JavaScript Júnior",
+    requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+    salario: 3000,
+    modalidade: "Presencial",
+    experienciaMinimaMeses: 2
+}
+];
